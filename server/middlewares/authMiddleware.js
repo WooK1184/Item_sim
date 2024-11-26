1415cb317ac6b60197e25fdb8bd261936d503509
WooK1184 <qkrtjddnr03@gmail.com>
2024-11-26 17:01:29 +0900

    feat : JWT 인증 미들웨어 작성
    베어러-토큰 형식 사용, 캐릭터 생성시 JWT 인증 사용 예정

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Unauthorized: Invalid token format' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const user = jwt.verify(token, JWT_SECRET);
+        req.locals = { user }; // 사용자 정보 저장
+        next();
+    } catch (error) {
+        return res.status(403).json({ error: 'Forbidden: Invalid or expired token' });
+    }
+};
+
+export default authMiddleware;

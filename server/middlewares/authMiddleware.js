b5556b294faec49cd985687067d08d3320f5b2f9
WooK1184 <qkrtjddnr03@gmail.com>
2024-11-27 16:48:18 +0900

    chore : login.js , authMiddleware.js 파일 수정
    dotenv 패키지 로드하도록 수정

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
         return res.status(401).json({ error: 'Unauthorized: Invalid token format' });
     }
 
